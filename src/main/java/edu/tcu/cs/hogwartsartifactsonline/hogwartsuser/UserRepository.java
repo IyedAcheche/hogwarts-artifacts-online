eba112bfcf94b5619f1f444b3f71dc0c1aca4929
Iyed Acheche <iyed.acheche@protonmail.com>
2025-04-09 19:28:21 -0500

    Remove hogwartsuser files to match reference repo structure

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<HogwartsUser, Integer> {
-} 
